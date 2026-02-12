981e7d78b1bf84791d0d66dd972d68e1fc4201dd
Lucas_M <59377840+D4ngerKitty@users.noreply.github.com>
2026-02-12 11:37:41 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -71,13 +71,15 @@ browserEvents.ArrowDown.onEvent(browserEvents.KeyEvent.Pressed, function () {
     if (!(menu_open)) {
         move_selector(4, "down")
     }
-    if (mainstartmenu) {
-        fancyText.setColor(sprites.readDataSprite(main_menu_text, "" + sprites.readDataNumber(main_menu_text, "menu_item_picked")), 4)
-        sprites.changeDataNumberBy(main_menu_text, "menu_item_picked", 1)
-        if (sprites.readDataNumber(main_menu_text, "menu_item_picked") == 5) {
-            sprites.setDataNumber(main_menu_text, "menu_item_picked", 1)
+    if (!(lockcontrols)) {
+        if (mainstartmenu) {
+            fancyText.setColor(sprites.readDataSprite(main_menu_text, "" + sprites.readDataNumber(main_menu_text, "menu_item_picked")), 4)
+            sprites.changeDataNumberBy(main_menu_text, "menu_item_picked", 1)
+            if (sprites.readDataNumber(main_menu_text, "menu_item_picked") == 5) {
+                sprites.setDataNumber(main_menu_text, "menu_item_picked", 1)
+            }
+            fancyText.setColor(sprites.readDataSprite(main_menu_text, "" + sprites.readDataNumber(main_menu_text, "menu_item_picked")), 3)
         }
-        fancyText.setColor(sprites.readDataSprite(main_menu_text, "" + sprites.readDataNumber(main_menu_text, "menu_item_picked")), 3)
     }
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -133,25 +135,66 @@ browserEvents.Z.onEvent(browserEvents.KeyEvent.Pressed, function () {
     if (!(menu_open)) {
         move_item()
     }
-    if (mainstartmenu) {
-        if (sprites.readDataNumber(main_menu_text, "menu_item_picked") == 1) {
-            mainstartmenu = false
-            sprites.destroy(main_menu_text)
-            sprites.destroy(sprites.readDataSprite(main_menu_text, "1"))
-            sprites.destroy(sprites.readDataSprite(main_menu_text, "2"))
-            sprites.destroy(sprites.readDataSprite(main_menu_text, "3"))
-            sprites.destroy(sprites.readDataSprite(main_menu_text, "4"))
-            timer.background(function () {
-                if (blockSettings.exists("checkpoint_zone_in_data")) {
-                    scene.cameraFollowSprite(cam)
-                    create_ui()
-                    canrun = true
-                    createlevel(blockSettings.readNumber("checkpoint_zone_in_data"))
-                    tiles.placeOnRandomTile(mySprite, assets.tile`myTile46`)
-                } else {
-                    START_CUTSENSE()
-                }
-            })
+    if (!(lockcontrols)) {
+        if (mainstartmenu) {
+            if (sprites.readDataNumber(main_menu_text, "menu_item_picked") == 1) {
+                mainstartmenu = false
+                sprites.destroy(main_menu_text)
+                sprites.destroy(sprites.readDataSprite(main_menu_text, "1"))
+                sprites.destroy(sprites.readDataSprite(main_menu_text, "2"))
+                sprites.destroy(sprites.readDataSprite(main_menu_text, "3"))
+                sprites.destroy(sprites.readDataSprite(main_menu_text, "4"))
+                timer.background(function () {
+                    if (blockSettings.exists("checkpoint_zone_in_data")) {
+                        scene.cameraFollowSprite(cam)
+                        create_ui()
+                        canrun = true
+                        createlevel(blockSettings.readNumber("checkpoint_zone_in_data"))
+                        tiles.placeOnRandomTile(mySprite, assets.tile`myTile46`)
+                    } else {
+                        START_CUTSENSE()
+                    }
+                })
+            } else if (sprites.readDataNumber(main_menu_text, "menu_item_picked") == 4) {
+                lockcontrols = true
+                fancyText.setColor(main_menu_text, 0)
+                pause(100)
+                fancyText.setColor(sprites.readDataSprite(main_menu_text, "1"), 0)
+                pause(100)
+                fancyText.setColor(sprites.readDataSprite(main_menu_text, "2"), 0)
+                pause(100)
+                fancyText.setColor(sprites.readDataSprite(main_menu_text, "3"), 0)
+                pause(100)
+                fancyText.setColor(sprites.readDataSprite(main_menu_text, "4"), 0)
+                pause(100)
+                blockSettings.clear()
+                game.reset()
+            } else if (sprites.readDataNumber(main_menu_text, "menu_item_picked") == 3) {
+                timer.background(function () {
+                    lockcontrols = true
+                    fancyText.setColor(sprites.readDataSprite(main_menu_text, "1"), 0)
+                    fancyText.setColor(sprites.readDataSprite(main_menu_text, "2"), 0)
+                    fancyText.setColor(sprites.readDataSprite(main_menu_text, "3"), 0)
+                    fancyText.setColor(sprites.readDataSprite(main_menu_text, "4"), 0)
+                    pause(200)
+                    fancyText.setText(sprites.readDataSprite(main_menu_text, "1"), "game made by")
+                    tiles.placeOnTile(sprites.readDataSprite(main_menu_text, "1"), tiles.getTileLocation(7, 3))
+                    fancyText.setColor(sprites.readDataSprite(main_menu_text, "1"), 2)
+                    pause(200)
+                    fancyText.setText(sprites.readDataSprite(main_menu_text, "2"), "DangerKitty")
+                    tiles.placeOnTile(sprites.readDataSprite(main_menu_text, "2"), tiles.getTileLocation(7, 4))
+                    fancyText.setColor(sprites.readDataSprite(main_menu_text, "2"), 2)
+                    pause(200)
+                    fancyText.setText(sprites.readDataSprite(main_menu_text, "3"), "playtesters gid")
+                    tiles.placeOnTile(sprites.readDataSprite(main_menu_text, "3"), tiles.getTileLocation(7, 5))
+                    fancyText.setColor(sprites.readDataSprite(main_menu_text, "3"), 2)
+                    pause(200)
+                    while (!(browserEvents.Z.isPressed())) {
+                        pause(10)
+                    }
+                    game.reset()
+                })
+            }
         }
     }
 })
@@ -2864,13 +2907,15 @@ browserEvents.ArrowUp.onEvent(browserEvents.KeyEvent.Pressed, function () {
     if (!(menu_open)) {
         move_selector(-4, "up")
     }
-    if (mainstartmenu) {
-        fancyText.setColor(sprites.readDataSprite(main_menu_text, "" + sprites.readDataNumber(main_menu_text, "menu_item_picked")), 4)
-        sprites.changeDataNumberBy(main_menu_text, "menu_item_picked", -1)
-        if (sprites.readDataNumber(main_menu_text, "menu_item_picked") == 0) {
-            sprites.setDataNumber(main_menu_text, "menu_item_picked", 4)
+    if (!(lockcontrols)) {
+        if (mainstartmenu) {
+            fancyText.setColor(sprites.readDataSprite(main_menu_text, "" + sprites.readDataNumber(main_menu_text, "menu_item_picked")), 4)
+            sprites.changeDataNumberBy(main_menu_text, "menu_item_picked", -1)
+            if (sprites.readDataNumber(main_menu_text, "menu_item_picked") == 0) {
+                sprites.setDataNumber(main_menu_text, "menu_item_picked", 4)
+            }
+            fancyText.setColor(sprites.readDataSprite(main_menu_text, "" + sprites.readDataNumber(main_menu_text, "menu_item_picked")), 3)
         }
-        fancyText.setColor(sprites.readDataSprite(main_menu_text, "" + sprites.readDataNumber(main_menu_text, "menu_item_picked")), 3)
     }
 })
 function hadle_items () {
@@ -3512,6 +3557,8 @@ let cam: Sprite = null
 let menu_open = false
 let can_open_menu = false
 let camFallowPlayer = false
+let lockcontrols = false
+lockcontrols = false
 camFallowPlayer = false
 stats.turnStats(true)
 can_open_menu = false
